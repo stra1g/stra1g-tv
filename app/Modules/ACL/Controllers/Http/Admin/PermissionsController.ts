4bd92162daa9556835828b65eb2d2e919b36c69f
Luis Vitor <luis.sg.vitor@gmail.com>
2022-03-02 15:02:48 -0300

    implements: schema to create permissions

--- app/Modules/ACL/Controllers/Http/Admin/PermissionsController.ts
+++ app/Modules/ACL/Controllers/Http/Admin/PermissionsController.ts
@@ -2,14 +2,18 @@ import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
 import { container } from 'tsyringe';
 
 import { CreatePermissionUseCase } from 'App/Modules/ACL/UseCases/Permission/CreatePermissionUseCase/CreatePermissionUseCase';
+import { PermissionValidator } from 'App/Modules/ACL/Validators/Permission';
 
 export default class PermissionsController {
-  public async store({ response, request }: HttpContextContract): Promise<void> {
-    const { method, resource, description } = request.body();
+  public async store(ctx: HttpContextContract): Promise<void> {
+    const { response, request } = ctx;
+
+    const validator = await new PermissionValidator.Store(ctx).createSchema();
+    const permissionData = await request.validate(validator);
 
     const createPermissionUseCase = container.resolve(CreatePermissionUseCase);
 
-    const permission = await createPermissionUseCase.execute({ method, resource, description });
+    const permission = await createPermissionUseCase.execute(permissionData);
 
     return response.json(permission);
   }
