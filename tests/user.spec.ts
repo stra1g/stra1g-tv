b04a4ab997c7635d1e8675e45dffc03d59c8fec9
Luis Vitor <luis.sg.vitor@gmail.com>
2022-03-04 23:41:01 -0300

    test: show user profile

--- tests/user.spec.ts
+++ tests/user.spec.ts
@@ -1,6 +1,7 @@
 import Env from '@ioc:Adonis/Core/Env';
 import supertest from 'supertest';
 import test from 'japa';
+import { UserFactory } from 'Database/factories';
 
 const BASE_URL = Env.get('APP_URL');
 
@@ -46,6 +47,48 @@ test.group('Functional: User', () => {
     });
 
     assert.equal(response.status, 400);
+    assert.property(response.body, 'message');
+    assert.property(response.body, 'status');
+    assert.property(response.body, 'showable');
+    assert.isBoolean(response.body.showable);
+    assert.isTrue(response.body.showable);
+  });
+
+  test('it should be able to show user profile', async (assert) => {
+    const password = '123456';
+    const user = await UserFactory.merge({ password }).create();
+
+    const {
+      body: { access_token: accessToken },
+    } = await supertest(BASE_URL).post('/sessions').send({
+      email: user.email,
+      password,
+    });
+
+    const response = await supertest(BASE_URL)
+      .get(`/users/${user.id}`)
+      .set('Authorization', `Bearer ${accessToken}`);
+
+    assert.equal(response.status, 200);
+    assert.property(response.body, 'id');
+  });
+
+  test('it should not be able to show user profile if user does not exists', async (assert) => {
+    const password = '123456';
+    const user = await UserFactory.merge({ password }).create();
+
+    const {
+      body: { access_token: accessToken },
+    } = await supertest(BASE_URL).post('/sessions').send({
+      email: user.email,
+      password,
+    });
+
+    const response = await supertest(BASE_URL)
+      .get(`/users/7329`)
+      .set('Authorization', `Bearer ${accessToken}`);
+
+    assert.equal(response.status, 404);
     assert.property(response.body, 'message');
     assert.property(response.body, 'status');
     assert.property(response.body, 'showable');
