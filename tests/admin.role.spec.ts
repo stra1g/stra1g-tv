db76b76b17961bcefeb092d6d058f51fa693f9b0
Luis Vitor <luis.sg.vitor@gmail.com>
2022-03-14 22:01:02 -0300

    test: sync permissions to a role

--- tests/admin.role.spec.ts
+++ tests/admin.role.spec.ts
@@ -1,5 +1,5 @@
 import Env from '@ioc:Adonis/Core/Env';
-import { RoleFactory } from 'Database/factories';
+import { PermissionFactory, RoleFactory } from 'Database/factories';
 import test from 'japa';
 import supertest from 'supertest';
 
@@ -58,5 +58,59 @@ test.group('Admin: Role', () => {
       .send(data)
       .set('Authorization', `Bearer ${accessToken}`)
       .expect(400);
+  });
+
+  test('it should be able to sync permissions to a role', async (assert) => {
+    const {
+      body: { access_token: accessToken },
+    } = await supertest(BASE_URL)
+      .post('/sessions')
+      .send({
+        email: 'admin@stra1g.com',
+        password: 'admin',
+      })
+      .expect(200);
+
+    const role = await RoleFactory.create();
+
+    const permissionIds = (await PermissionFactory.createMany(2)).map(
+      (permission) => permission.id
+    );
+
+    const response = await supertest(BASE_URL)
+      .put(`/admin/roles/${role.id}/sync/permissions`)
+      .send({
+        permission_ids: permissionIds,
+      })
+      .set('Authorization', `Bearer ${accessToken}`)
+      .expect(200);
+
+    assert.property(response.body, 'permissions');
+    assert.isArray(response.body.permissions);
+    assert.lengthOf(response.body.permissions, 2);
+  });
+
+  test('it should not be able to sync permissions to a role if role does not exists', async () => {
+    const {
+      body: { access_token: accessToken },
+    } = await supertest(BASE_URL)
+      .post('/sessions')
+      .send({
+        email: 'admin@stra1g.com',
+        password: 'admin',
+      })
+      .expect(200);
+
+    const permissionIds = (await PermissionFactory.createMany(2)).map(
+      (permission) => permission.id
+    );
+
+    await supertest(BASE_URL)
+      .put(`/admin/roles/739/sync/permissions`)
+      .send({
+        permission_ids: permissionIds,
+      })
+      .set('Authorization', `Bearer ${accessToken}`)
+      .expect(404);
   });
 });
