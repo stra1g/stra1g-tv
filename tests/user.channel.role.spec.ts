cc2dd8fb061b230ff8f999cc061a91bdc5dea2e7
Luis Vitor <luis.sg.vitor@gmail.com>
2022-03-30 21:19:40 -0300

    test(userChannelRole): update user channel role

--- tests/user.channel.role.spec.ts
+++ tests/user.channel.role.spec.ts
@@ -96,4 +96,47 @@ test.group('Functional: User channel role', (group) => {
     assert.property(response.body.data[0], 'role');
     assert.property(response.body.data[0], 'created_at');
   });
+
+  test('it should be able to update user channel role', async (assert) => {
+    const user = await UserFactory.merge({ password: '123456' }).create();
+    const {
+      body: { access_token: accessToken },
+    } = await supertest(BASE_URL)
+      .post('/sessions')
+      .send({
+        email: user.email,
+        password: '123456',
+      })
+      .expect(200);
+
+    const channel = await ChannelFactory.merge({ userId: user.id }).create();
+
+    const channelRole = await ChannelRoleFactory.create();
+
+    const anotherUser = await UserFactory.create();
+    await channelRole.related('channels').attach({
+      [channel.id]: {
+        user_id: anotherUser.id,
+      },
+    });
+
+    const anotherChannelRole = await ChannelRoleFactory.create();
+
+    const data = {
+      channel_id: channel.id,
+      channel_role_id: anotherChannelRole.id,
+      user_id: anotherUser.id,
+    };
+
+    const response = await supertest(BASE_URL)
+      .put('/channels/user/roles')
+      .send(data)
+      .set('Authorization', `Bearer ${accessToken}`)
+      .expect(200);
+
+    assert.equal(
+      response.body.message,
+      i18n.formatMessage('messages.success.successfully_assigned_role')
+    );
+  });
 });
